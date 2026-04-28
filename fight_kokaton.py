d2233f914949aff9af140084e95665a2aa2330e6
kazuki <c0c240600f@edu.teu.ac.jp>
2026-04-28 17:23:01 +0900

    演習４

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -1,9 +1,10 @@
+import math
 import os
 import random
 import sys
 import time
 import pygame as pg
-
+ 
 
 WIDTH = 1100  # ゲームウィンドウの幅
 HEIGHT = 650  # ゲームウィンドウの高さ
@@ -56,6 +57,7 @@ class Bird:
         self.img = __class__.imgs[(+5, 0)]
         self.rct: pg.Rect = self.img.get_rect()
         self.rct.center = xy
+        self.dire = (+5, 0)
 
     def change_img(self, num: int, screen: pg.Surface):
         """
@@ -83,7 +85,9 @@ class Bird:
         if not (sum_mv[0] == 0 and sum_mv[1] == 0):
             self.img = __class__.imgs[tuple(sum_mv)]
         screen.blit(self.img, self.rct)
-
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self.img = __class__.imgs[tuple(sum_mv)]
+            self.dire = tuple(sum_mv)
 
 class Beam:
     """
@@ -94,11 +98,17 @@ class Beam:
         ビーム画像Surfaceを生成する
         引数 bird：ビームを放つこうかとん（Birdインスタンス）
         """
-        self.img = pg.image.load(f"fig/beam.png")
+        img = pg.image.load(f"fig/beam.png")
+        # self.rct = self.img.get_rect()
+        # self.rct.centery = bird.rct.centery
+        # self.rct.left = bird.rct.left
+        self.vx, self.vy = bird.dire
+        angle = math.degrees(math.atan2(-self.vy, self.vx))
+        self.img = pg.transform.rotozoom(img, angle, 1.0)
         self.rct = self.img.get_rect()
-        self.rct.centery = bird.rct.centery
-        self.rct.left = bird.rct.left
-        self.vx, self.vy = +5, 0
+        self.rct.centerx = bird.rct.centerx + bird.rct.width * self.vx // 5
+        self.rct.centery = bird.rct.centery + bird.rct.height * self.vy // 5
+
 
     def update(self, screen: pg.Surface):
         """
